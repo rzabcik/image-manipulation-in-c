1ef9c9b61815cac138f8b5d603b6465bcb3e417b
Matt Broussard <matt@mattb.io>
2015-11-29 18:18:15 -0600

    add scalability test to gen_runner

--- gen_runner.py
+++ gen_runner.py
@@ -21,14 +21,32 @@ output_filename = "times.csv"
 print "#!/bin/sh"
 print "echo \"operation,filename,width,height,time_ns,n_threads\" > %s" % output_filename
 
-i = 0
-num_ops = len(ops) * len(images) * repetitions
+def regular_test():
+    i = 0
+    num_ops = len(ops) * len(images) * repetitions
 
-for im in images:
-    for _ in xrange(repetitions):
-        for op in ops:
-            print "%s %s%s >> %s" % (op, image_path, im, output_filename)
-            i += 1
-            print "echo \"%d of %d runs completed (%s %s).\"" % (i, num_ops, op, im)
+    for im in images:
+        for _ in xrange(repetitions):
+            for op in ops:
+                print "%s %s%s >> %s" % (op, image_path, im, output_filename)
+                i += 1
+                print "echo \"%d of %d runs completed (%s %s).\"" % (i, num_ops, op, im)
 
-print "echo \"Done. Output in %s\"" % output_filename
+    print "echo \"Done. Output in %s\"" % output_filename
+
+def rotate_scalability_test():
+    op = "./profile_rotate"
+    import multiprocessing
+    cores = multiprocessing.cpu_count()
+
+    i = 0
+    num_ops = len(images) * repetitions * cores
+    for im in images:
+        for _ in xrange(repetitions):
+            for n_threads in xrange(1,cores+1):
+                print "%s %s%s %d >> %s" % (op, image_path, im, n_threads, output_filename)
+                i += 1
+                print "echo \"%d of %d runs completed (%s %s %d).\"" % (i, num_ops, op, im, n_threads)
+
+# regular_test()
+rotate_scalability_test()
